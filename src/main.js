f4d0da291631a89f7f6146a8d1b2dfc3a3bf21a7
Ines Touzi <31033883+ineso@users.noreply.github.com>
2022-05-01 19:24:34 +0200

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// Copyright Carrefour(2022)
+//
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import 'v-calendar/dist/style.css'
+
+const app = createApp(App)
+
+app.mount('#app')
