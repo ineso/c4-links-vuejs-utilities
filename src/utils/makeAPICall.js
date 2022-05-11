e3f688f03e639aa710c27c6d859dd67e42432fe1
Ines Touzi <31033883+ineso@users.noreply.github.com>
2022-05-11 06:58:35 +0200

    Add files via upload

--- src/utils/makeAPICall.js
+++ src/utils/makeAPICall.js
@@ -1,21 +1,14 @@
 // Copyright Carrefour(2022)
 //
 
-import each from 'lodash-es/each'
-import storage from './storage'
-
 const makeAPIFetch = (endpoint, params, options) => {
-  const authToken = storage.cookie.get('Carrefour-accessToken')
-  if (!authToken && options.catch498) {
-    return Promise.resolve({ status: 498 })
-  }
   return fetch(process.env.VUE_APP_API_BASE_URL + endpoint, {
     ...params,
+    credentials: 'include',
     body: params.body ? JSON.stringify(params.body) : null,
     headers: new Headers({
       Accept: 'application/json',
       'Content-Type': 'application/json',
-      Authorization: authToken,
       ...params.headers || {}
     })
   })
@@ -31,31 +24,6 @@ const makeAPIFetch = (endpoint, params, options) => {
  */
 const makeAPICall = (endpoint, params = {}, { catch498 = true } = {}) => {
   return makeAPIFetch(endpoint, params, { catch498 }).then(response => {
-    const refreshToken = storage.cookie.get('Carrefour-refreshToken')
-    if (catch498 && response.status === 498 && refreshToken) {
-      return makeAPICall('/oidc/refresh', {
-        method: 'POST',
-        headers: {
-          Authorization: refreshToken
-        }
-      }, { catch498: false }).then(data => {
-        if (data.accessToken) {
-          each(['accessToken', 'refreshToken'], type => {
-            if (data[type]) {
-              storage.cookie.set(
-                [data.instanceId, type].join('-'),
-                data[type].token,
-                { maxAge: data[type].maxAge / 1000 }
-              )
-            }
-          })
-
-          return makeAPICall(endpoint, params, { catch498: false })
-        }
-
-        return Promise.reject(response)
-      })
-    }
     if (response.ok) {
       return response.json()
     }
@@ -64,3 +32,4 @@ const makeAPICall = (endpoint, params = {}, { catch498 = true } = {}) => {
 }
 
 export default makeAPICall
+
